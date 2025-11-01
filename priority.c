cdbcd6633fc60a161c317dc00cf433cb69d38943
ambiliambaran <ambiliambaran191sa19@gmail.com>
2025-11-01 16:27:42 +0530

    priority scheduling

--- priority.c
+++ priority.c
@@ -1,62 +1,52 @@
-#include <stdio.h>
-
-int main() {
-    int n, bt[20], pri[20], p[20], wt[20], tat[20], i, j, temp;
-    float avg_wt = 0, avg_tat = 0;
-
-    printf("Enter number of processes: ");
-    scanf("%d", &n);
-
-    for(i = 0; i < n; i++) {
-        printf("Enter Burst Time and Priority for P%d: ", i + 1);
-        scanf("%d %d", &bt[i], &pri[i]);
-        p[i] = i + 1;
+#include<stdio.h>
+int main()
+{
+  int n,i,j,temp;
+  int bt[20],wt[20],tat[20],p[20],pri[20];
+  int total_wt=0,total_tat=0;
+  
+  printf("Enter the number of processes : ");
+  scanf("%d",&n);
+   for(i=0;i<n;i++){
+  printf("Enter Burst time and Priority for P%d : ",i+1);
+ 
+    scanf("%d %d",&bt[i],&pri[i]);
+    p[i]=i+1;
+  }
+  for(i=0;i<n-1;i++){
+     for(j=i+1;j<n;j++){
+       if(pri[i]>pri[j]){ 
+          temp=pri[i];
+          pri[i]=pri[j];
+          pri[j]=temp;
+          
+          temp=bt[i];
+          bt[i]=bt[j];
+          bt[j]=temp;
+          
+          temp=p[i];
+          p[i]=p[j];
+          p[j]=temp;
+       }
     }
-
-    // Sorting according to priority (lower number = higher priority)
-    for(i = 0; i < n - 1; i++) {
-        for(j = i + 1; j < n; j++) {
-            if(pri[i] > pri[j]) {
-                temp = pri[i];
-                pri[i] = pri[j];
-                pri[j] = temp;
-
-                temp = bt[i];
-                bt[i] = bt[j];
-                bt[j] = temp;
-
-                temp = p[i];
-                p[i] = p[j];
-                p[j] = temp;
-            }
-        }
-    }
-
-    // Waiting time for first process = 0
-    wt[0] = 0;
-
-    // Calculate waiting times
-    for(i = 1; i < n; i++) {
-        wt[i] = wt[i - 1] + bt[i - 1];
-    }
-
-    // Calculate turnaround times
-    for(i = 0; i < n; i++) {
-        tat[i] = bt[i] + wt[i];
-        avg_wt += wt[i];
-        avg_tat += tat[i];
-    }
-
-    avg_wt /= n;
-    avg_tat /= n;
-
-    printf("\nProcess\tBurst Time\tPriority\tWaiting Time\tTurnaround Time\n");
-    for(i = 0; i < n; i++) {
-        printf("P%d\t%d\t\t%d\t\t%d\t\t%d\n", p[i], bt[i], pri[i], wt[i], tat[i]);
-    }
-
-    printf("\nAverage Waiting Time: %.2f", avg_wt);
-    printf("\nAverage Turnaround Time: %.2f\n", avg_tat);
-
-    return 0;
+  }
+  wt[0]=0;
+  for(i=1;i<n;i++){
+    wt[i]=wt[i-1]+bt[i-1];
+  }
+  for(i=0;i<n;i++){
+    tat[i]=wt[i]+bt[i];
+  }
+  printf("\nProcess\tPriority\tBurst_time\tWaiting_time\tTurnAround_time\n");
+  for(i=0;i<n;i++){
+    printf("\nP%d\t\t%3d\t\t%3d\t\t%3d\t\t%3d\n",p[i],pri[i],bt[i],wt[i],tat[i]);
+       total_wt+=wt[i];
+       total_tat+=tat[i];
+  }
+  printf("\nAverage waiting time : %.2f ",(float)total_wt/n);
+  printf("\nAverage TurnAround time : %.2f  ",(float)total_tat/n);
+  return 0;
 }
+    
+       
+        
